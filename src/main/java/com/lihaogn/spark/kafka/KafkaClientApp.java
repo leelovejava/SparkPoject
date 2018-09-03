9e036e41c1d666171e9c8ef9f75e3f56a9d0fe96
lihaogn <lihaogn@163.com>
2018-09-03 15:45:54 +0800

    commit first

---
+++ src/main/java/com/lihaogn/spark/kafka/KafkaClientApp.java
@@ -0,0 +1,15 @@
+package com.lihaogn.spark.kafka;
+
+/**
+ * kafka 测试
+ */
+public class KafkaClientApp {
+    public static void main(String[] args) {
+
+        new KafkaProducer(KafkaProperties.TOPIC).start();
+
+        new KafkaConsumer(KafkaProperties.TOPIC).start();
+
+
+    }
+}
