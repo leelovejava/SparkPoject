d0a80398a2924ac1c658bc2002abc929366a66fe
tianhao <1165746141@qq.com>
2018-11-20 14:46:03 +0800

    修改host

--- src/test/java/com.lihaogn.sparkTest/kafka/KafkaProperties.java
+++ src/test/java/com.lihaogn.sparkTest/kafka/KafkaProperties.java
@@ -5,8 +5,8 @@ package com.lihaogn.sparkTest.kafka;
  */
 public class KafkaProperties {
 
-    public static final String ZK = "localhost:2181";
+    public static final String ZK = "hadoop000:2181";
     public static final String TOPIC = "test_topic";
-    public static final String BROKER_LIST = "localhost:9092";
+    public static final String BROKER_LIST = "hadoop000:9092";
     public static final String GROUP_ID = "test_group1";
 }
